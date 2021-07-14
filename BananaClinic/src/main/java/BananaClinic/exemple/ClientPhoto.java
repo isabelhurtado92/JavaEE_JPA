e08a88200636257d3a4d9c4e163b95abd58408bf
admin CIFO <admin CIFO@DESKTOP-K9FH0BE>
2021-07-14 13:49:59 +0200

    mongodb falta extend mongorepo

--- BananaClinic/src/main/java/BananaClinic/exemple/ClientPhoto.java
+++ BananaClinic/src/main/java/BananaClinic/exemple/ClientPhoto.java
@@ -1,9 +1,58 @@
 package BananaClinic.exemple;
-
+import javax.persistence.Id;
+import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document (collection= "clientPhotos")
 
 public class ClientPhoto {
+
+	@Id
+private String Id;
+private String name;
+private Binary photo;
+
+//Constructor:
+
+public ClientPhoto(String id, String name, Binary photo) {
+	super();
+	Id = id;
+	this.name = name;
+	this.photo = photo;
+}
+
+
+public ClientPhoto() {
+	super();
+}
+
+//Getters and Setters:
+
+public String getId() {
+	return Id;
+}
+public void setId(String id) {
+	Id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Binary getPhoto() {
+	return photo;
+}
+public void setPhoto(Binary photo) {
+	this.photo = photo;
+}
+
+
+@Override
+public String toString() {
+	return "ClientPhoto [name=" + name + ", photo=" + photo + "]";
+}
+
+
 
 }
--- BananaClinic/src/main/java/BananaClinic/exemple/EmployeePhoto.java
+++ BananaClinic/src/main/java/BananaClinic/exemple/EmployeePhoto.java
@@ -1,9 +1,74 @@
 package BananaClinic.exemple;
 
+import javax.persistence.Id;
+
+import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document (collection = "employeePhotos")
 
 public class EmployeePhoto {
+	
+	@Id
+	private String id;
+	private String name;
+	private Binary photo;
+	
+	
+	//constructor's not necessary but anyway:
+	
+	
+	public EmployeePhoto(String name, Binary image) {
+		super();
+		this.name = name;
+		this.image = image;
+	}
 
+
+	public EmployeePhoto() {
+		super();
+	}
+
+	
+	
+	//Getters and setters:
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Binary getImage() {
+		return image;
+	}
+
+
+	public void setImage(Binary image) {
+		this.image = image;
+	}
+
+
+	@Override
+	public String toString() {
+		return "EmployeePhoto [name=" + name + ", image=" + image + "]";
+	}
+	
+	
+	
 }
