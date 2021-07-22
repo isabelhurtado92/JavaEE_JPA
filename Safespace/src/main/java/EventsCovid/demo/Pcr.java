2b5fb301826a5e9a8c8594859848f2e6c47ed3fb
admin CIFO <admin CIFO@DESKTOP-K9FH0BE>
2021-07-22 13:13:50 +0200

    c1

---
+++ Safespace/src/main/java/EventsCovid/demo/Pcr.java
@@ -0,0 +1,110 @@
+package EventsCovid.demo;
+
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Table
+public class Pcr {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int Id;
+@DateTimeFormat(pattern = "yyyy-MM-dd")
+	public Date extractionDate;
+	public boolean result;
+	public String ProfessionalValidation;
+
+//Relationship type:
+@ManyToOne
+@JoinColumn (name = "Guest_ID")
+public Guest guest;	
+	
+	
+//Constructor:			
+	public Pcr(Date extractionDate, boolean result, String ProfessionalValidation) {
+		super();
+		//Id = id; 	 not necessary JPA's annotation "@id" has already created it.
+		this.extractionDate = extractionDate;
+		this.result = result;
+		this.ProfessionalValidation=ProfessionalValidation;
+	}
+
+	public Pcr() {
+		super();
+	}
+
+
+	//Getters and Setters:		
+	public int getId() {
+		return Id;
+	}
+
+
+	public void  setId(int id) {
+		Id = id;
+	}
+
+
+	public Date getExtractionDate() {
+		return extractionDate;
+	}
+
+
+	public void setExtractionDate(Date extractionDate) {
+		this.extractionDate = extractionDate;
+	}
+
+	
+	
+	public boolean isResult() {
+		return result;
+	}
+
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+
+	public String getProfessionalValidation() {
+		return ProfessionalValidation;
+	}
+
+	public void setProfessionalValidation(String professionalValidation) {
+		ProfessionalValidation = professionalValidation;
+	}
+
+	public Guest getGuest() {
+		return guest;
+	}
+
+	public void setGuest(Guest guest) {
+		this.guest = guest;
+	}
+	
+	//adding method:
+	
+	/*
+	 * public void addGuest(Guest guest) { this.guest.add(guest);
+	 * guest.setPcr(this); }
+	 */
+	
+
+	
+	//ToString:	
+	@Override
+	public String toString() {
+		return "Pcr [Id=" + Id + ", ExtractionDate=" + extractionDate + ", Result=" + result + ", Guest=" + guest + ", ProfessionalValidation=\" + professionalValidation + \"]";
+	}
+
+}
---
+++ Safespace/src/main/java/EventsCovid/demo/PcrController.java
@@ -0,0 +1,101 @@
+package EventsCovid.demo;
+
+
+	import java.util.Optional;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.RequestMapping;
+
+	@Controller
+	@RequestMapping("/pcr")
+
+	public class PcrController {
+			
+	@Autowired
+	PcrRepository pcrRepository;
+			
+		//CRUD OPS:
+
+		//READ
+		@RequestMapping("/allPcr")
+			public Iterable<Pcr> getAllPcr() {
+
+				return pcrRepository.findAll();
+			}
+		
+			//  CREATE 
+			
+			@RequestMapping("/newPcr")
+			public String newPcr() {
+
+				return "";
+			}
+			
+			@RequestMapping("/addPcr")
+			public String insertPcr(Pcr pcr) {
+				
+				pcrRepository.save(pcr);
+				
+				return "";
+			}
+		
+			// READ 
+			@RequestMapping("/getPcr/{id}")
+			public Pcr findById(@PathVariable int id) {
+
+				Optional<Pcr> pcrFound = pcrRepository.findById(id);
+
+				if (pcrFound.isPresent()) {
+
+					return pcrFound.get();
+				}
+
+				return null;
+			}
+			
+		
+
+			//  DELETE
+			@RequestMapping("/deletePcr/{id}")
+			public void deletePcr(@PathVariable int id) {
+
+				Optional<Pcr> guestFound = pcrRepository.findById(id);
+
+				if (pcrFound.isPresent()) {
+
+					pcrRepository.deleteById(id);
+				}
+
+			}
+
+			// UPADATE 
+			@RequestMapping("/updatePcr/{id}")
+			public void updatePcr(Pcr pcr, @PathVariable int id) {
+
+				Optional<Pcr> pcrFound = pcrRepository.findById(id);
+
+				if (pcrFound.isPresent()) {
+
+					if (!pcr.getExtractionDate().equals(pcrFound.get().getExtractionDate()))
+						pcrFound.get().setExtractionDate(pcr.getExtractionDate());
+					
+					/*
+					 * if (!pcr.isResult().equals(pcrFound.get().isResult()))
+					 * pcrFound.get().setResult(pcr.isResult());
+					 */
+
+					if (pcr.getProfessionalValidation() != pcrFound.get().getProfessionalValidation())
+						pcrFound.get().setProfessionalValidation(pcr.getProfessionalValidation());
+
+
+					pcrRepository.save(pcrFound.get());
+				}
+			}
+			
+		}
+		
+
+
+
